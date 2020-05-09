641e069204c81b21ae6e8e66fbcdd8b4c4c77ac5
eleonoradgr <eleonora.digregorio8@gmail.com>
2020-05-09 16:59:01 +0200

    dcmergesort working properly

--- DCTemplate.cpp
+++ DCTemplate.cpp
@@ -7,13 +7,13 @@
 
 template<typename Tin, typename Tout>
 Tout dc_seq(Tin input,
-        bool (*basecase)(Tin),
-        Tout (*solve)(Tin),
-        std::vector<Tin> (*divide)(Tin),
-        Tout (*conquer)(std::vector<Tout>)) {
+            bool (*basecase)(Tin),
+            Tout (*solve)(Tin),
+            std::vector<Tin> (*divide)(Tin),
+            Tout (*conquer)(std::vector<Tout>)) {
 
-    if(basecase(input)) {
-        return(solve(input));
+    if (basecase(input)) {
+        return (solve(input));
     } else {
         auto subproblems = divide(input);
 
@@ -22,11 +22,11 @@ Tout dc_seq(Tin input,
                        subproblems.begin(),
                        [&](Tin x) {
                            auto res = dc_seq(x, basecase, solve, divide, conquer);
-                           return(res);
+                           return (res);
                        });
 
         auto result = conquer(subproblems);
-        return(result);
+        return (result);
     }
 };
 
@@ -39,43 +39,43 @@ Tout dc_par(Tin input,
             int nw) {
 
     std::vector<Tin> tasks;
-    tasks.reserve(nw*2);
+    tasks.reserve(nw * 2);
     tasks.push_back(input);
     std::atomic<int> remaining(0);
 
 
-    while (tasks.size() < nw){
+    while (tasks.size() < nw) {
         std::vector<Tin> tmp;
-        tmp.reserve(nw*2);
-        for (int i = 0; i < tasks.size(); ++i){
+        tmp.reserve(nw * 2);
+        for (int i = 0; i < tasks.size(); ++i) {
             std::vector<Tin> aus = divide(tasks[i]);
-           tmp.insert(tmp.end(), aus.begin(), aus.end());
+            tmp.insert(tmp.end(), aus.begin(), aus.end());
         }
         tmp.swap(tasks);
     }
 
     remaining = tasks.size();
     std::vector<Tout> outputs(tasks.size());
-    std::vector<std::thread*>  tid;
+    std::vector<std::thread *> tid;
     tid.reserve(nw);
-    auto body = [&](){
-        while(remaining>=0){
-            Tin my_task = remaining.fetch_sub(1);
+    auto body = [&]() {
+        while (remaining >= 0) {
+            int my_task = remaining.fetch_sub(1);
             my_task--;
-            if (my_task>=0){
-                outputs[my_task] = dc_seq(tasks[my_task],basecase,solve,divide,conquer);
+            if (my_task >= 0) {
+                outputs[my_task] = dc_seq(tasks[my_task], basecase, solve, divide, conquer);
             }
         }
     };
 
-    for(int i=0; i<nw; i++)
+    for (int i = 0; i < nw; i++)
         tid.push_back(new std::thread(body));
 
-    for(auto &t : tid)
+    for (auto &t : tid)
         t->join();
 
     auto result = conquer(outputs);
-    return(result);
+    return (result);
 
 };
 
--- dcmergesort.cpp
+++ dcmergesort.cpp
@@ -1,44 +1,47 @@
 /**
   Mergesort: sort an array of N integer in parallel using the DAC pattern
 
-  Author: Tiziano De Matteis <dematteis@di.unipi.it>
+  Part of the code was written by Tiziano De Matteis <dematteis@di.unipi.it>
  */
 #include <iostream>
 #include <functional>
 #include <vector>
 #include <algorithm>
 #include <cstring>
+#include <climits>
 #include "utils/utimer.hpp"
+#include "DCTemplate.cpp"
+#include "assert.h"
 
-#define CUTOFF 2000
+#define CUTOFF 5000
 
 /* ------------------------------------------------------------------ */
 //maximum value for arrays elements
-const int MAX_NUM=99999.9;
+const int MAX_NUM = 99999.9;
 const int SEED = 1234;
 
 static bool isArraySorted(int *a, int n) {
-    for(int i=1;i<n;i++)
-        if(a[i]<a[i-1])
+    for (int i = 1; i < n; i++)
+        if (a[i] < a[i - 1])
             return false;
     return true;
 }
 
 static int *generateRandomArray(int n) {
-    srand (SEED);
-    int *numbers=new int[n];
-    for(int i=0;i<n;i++)
-        numbers[i]=(int) (rand()) / ((RAND_MAX/MAX_NUM));
+    srand(SEED);
+    int *numbers = new int[n];
+    for (int i = 0; i < n; i++)
+        numbers[i] = (int) (rand()) / ((RAND_MAX / MAX_NUM));
     return numbers;
 }
 /* ------------------------------------------------------------------ */
 
 
 // Operand and Results share the same format
-struct ops{
-    int *array=nullptr;		    //array (to sort/sorted)
-    int left=0;                     //left index
-    int right=0;                    //right index
+struct ops {
+    int *array = nullptr;            //array (to sort/sorted)
+    int left = 0;                     //left index
+    int right = 0;                    //right index
 };
 
 typedef struct ops Operand;
@@ -49,86 +52,91 @@ typedef struct ops Result;
  * The divide simply 'split' the array in two: the splitting is only logical.
  * The recursion occur on the left and on the right part
  */
-void divide(const Operand &op,std::vector<Operand> &subops)
-{
-    int mid=(op.left+op.right)/2;
+std::vector<Operand> divide(const Operand op) {
+    std::vector<Operand> subops;
+    subops.reserve(2);
+    int mid = (op.left + op.right) / 2;
     Operand a;
-    a.array=op.array;
-    a.left=op.left;
-    a.right=mid;
+    a.array = op.array;
+    a.left = op.left;
+    a.right = mid;
     subops.push_back(a);
 
     Operand b;
-    b.array=op.array;
-    b.left=mid+1;
-    b.right=op.right;
+    b.array = op.array;
+    b.left = mid + 1;
+    b.right = op.right;
     subops.push_back(b);
+    return subops;
 }
 
 
 /*
  * For the base case we resort to std::sort
  */
-void seq(const Operand &op, Result &ret)
-{
+Result seq(const Operand op) {
 
-    std::sort(&(op.array[op.left]),&(op.array[op.right+1]));
+    std::sort(&(op.array[op.left]), &(op.array[op.right + 1]));
 
     //the result is essentially the same of the operand
-    ret=op;
+    return const_cast<Result &>(op);
 }
 
 
 /*
- * The Merge (Combine) function start from two ordered sub array and construct the original one
+ * The Merge (Combine) function start from two or more ordered sub array and construct the original one
  * It uses additional memory
  */
-void mergeMS(std::vector<Result>&ress, Result &ret)
-{
+Result mergeMS(std::vector<Result> ress) {
     //compute what is needed: array pointer, mid, ...
-    int *a=ress[0].array;                 //get the array
-    int mid=ress[0].right;                //by construction
-    int left=ress[0].left, right=ress[1].right;
-    int size=right-left+1;
-    int *tmp=new int[size];
-    int i=left,j=mid+1;
+    int *a = ress.front().array;
+    int left = ress.front().left;
+    int right = ress.back().right;
+    int size = right - left + 1;
+    int *tmp = new int[size];
+
+    Result ret;
+
+    std::vector<int> indexes(ress.size());
+    for (auto i = 0; i < ress.size(); ++i) {
+        indexes[i] = ress[i].left;
+    }
 
     //merge in order
-    for(int k=0;k<size;k++)
-    {
-        if(i<=mid && (j>right || a[i]<= a[j]))
-        {
-            tmp[k]=a[i];
-            i++;
+    for (int k = 0; k < size; k++) {
+        int min = INT_MIN;
+        int min_ind = -1;
+        for (int i = 0; i < indexes.size(); ++i) {
+            if (indexes[i] <= ress[i].right && ress[0].array[indexes[i]] < min) {
+                min = ress[0].array[indexes[i]];
+                min_ind = i;
+            }
         }
-        else
-        {
-            tmp[k]=a[j];
-            j++;
-        }
+        tmp[k] = min;
+        indexes[min_ind]++;
     }
 
     //copy back
-    memcpy(a+left,tmp,size*sizeof(int));
+    memcpy(a + left, tmp, size * sizeof(int));
 
     delete[] tmp;
 
     //build the result
-    ret.array=a;
-    ret.left=left;
-    ret.right=right;
+    ret.array = a;
+    ret.left = left;
+    ret.right = right;
+    return ret;
 }
 
 
 /*
  * Base case condition
  */
-bool cond(const Operand &op)
-{
-    return (op.right-op.left<=CUTOFF);
+bool cond(Operand op) {
+    return (op.right - op.left <= CUTOFF);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     if (argc < 3) {
         std::cout << "usage: " << argv[0] << std::endl;
         std::cout << "n : length of random vector to generate (mandatory)" << std::endl;
@@ -138,19 +146,31 @@ int main(int argc, char *argv[]){
     int n = atoi(argv[1]);
     int nw = atoi(argv[2]);
     int *array = generateRandomArray(n);
+    Operand oper;
+    oper.array = array;
+    oper.left = 0;
+    oper.right = n - 1;
+    Result res;
 
     {
-        utimer omp(" Sequential execution for "+ std::to_string(fib) + "th number \n");
-        fibonacci = dc_seq<int,int>(fib, &cond, &solve, &divide, &combine);
+        utimer omp(" Sequential execution for " + std::to_string(n) + "th number \n");
+        res = dc_seq<Operand, Result>(oper, &cond, &seq, &divide, &mergeMS);
     }
-    std::cout << "Fibonacci value: " << fibonacci << std::endl;
-    int *array = generateRandomArray(n);
-    {
-        utimer omp(" Parallel execution for "+ std::to_string(fib) + "th number with "+ std::to_string(nw) + " trheads \n");
-        fibonacci = dc_par(fib, &cond, &solve, &divide, &combine,4);
-    }
-    std::cout << "Fibonacci value: " << fibonacci << std::endl;
+    assert(isArraySorted(res.array, n));
+    std::cout << " Sorted: " << isArraySorted(res.array, n) << std::endl;
+    free(array);
+    array = generateRandomArray(n);
+    oper.array = array;
+    oper.left = 0;
+    oper.right = n - 1;
 
+    {
+        utimer omp(" Parallel execution for " + std::to_string(n) + "th number with " + std::to_string(nw) +
+                   " trheads \n");
+        res = dc_par(oper, &cond, &seq, &divide, &mergeMS, nw);
+    }
+    std::cout << " Sorted: " << isArraySorted(res.array, n) << std::endl;
+    assert(isArraySorted(res.array, n));
 
 }
 
